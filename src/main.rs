cf8e60934f74fa1f1aaec7b92dc7fbf47d9430c1
Chance Addis <chance.h.addis@proton.me>
2025-11-21 00:19:23 -0800

    chore: add packings.json, write `solutions.json` instead of `results.json`

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let packings = filterer.find_packings();
 
     let realizer = Realizer::new(ANSWERS, GUESSES);
-    let results = realizer.realize(&packings);
+    let solutions = realizer.realize(&packings);
 
     println!(
         "There are {} (optimally bad) wordle solutions.",
-        results.len()
+        solutions.len()
     );
-    write(&results, "results.json")?;
+    write(&packings, "packings.json")?;
+    write(&solutions, "solutions.json")?;
 
     Ok(())
 }
 
-/// Writes the serialized `BadWordleSolution`s to a file.
+/// Writes serializable data to a file.
 fn write<T: serde::Serialize>(data: &T, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
     let file = std::fs::File::create(filename)?;
     let writer = std::io::BufWriter::new(file);
-    serde_json::to_writer(writer, data)?;
+    serde_json::to_writer_pretty(writer, data)?;
     Ok(())
 }
