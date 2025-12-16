5799e1f01ccbc22472466c3ff3194652f14306d6
Chance Addis <chance.h.addis@proton.me>
2025-12-16 01:57:31 -0800

    style: make `write` prettier

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-/// Writes serializable data to a file.
-fn write<T: serde::Serialize>(data: &T, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
+/// Write serializable data to a JSON file with pretty formatting.
+fn write<T>(data: &T, filename: &str) -> Result<(), Box<dyn std::error::Error>>
+where
+    T: serde::Serialize,
+{
     let file = std::fs::File::create(filename)?;
     let writer = std::io::BufWriter::new(file);
     serde_json::to_writer_pretty(writer, data)?;
